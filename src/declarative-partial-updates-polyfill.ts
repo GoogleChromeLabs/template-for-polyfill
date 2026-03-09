31b13fcf70cff387ea15f82b36d646d4f86bf60a
Barry Pollard <barrypollard@google.com>
2026-03-09 03:15:37 +0000

    More test cases

--- src/declarative-partial-updates-polyfill.ts
+++ src/declarative-partial-updates-polyfill.ts
@@ -27,11 +27,12 @@
     let startNode: Comment | null = null;
     let node: Comment | null;
     let depth = 0;
+    let startNodeDepth = 0;
 
     while ((node = walker.nextNode() as Comment)) {
       const data = node.data.trim();
 
-      // CASE 1: We are looking for the start of a range or a simple marker
+      // CASE 1: We are looking for a simple marker
       if (data.startsWith('?marker')) {
         const isMatch = hash
           ? data.includes(`?marker name="${hash}"`)
@@ -44,7 +45,7 @@
           return;
         }
       }
-      // CASE 2: We are looking for the start of a range or a simple marker
+      // CASE 2: We are looking for the start of a range
       if (data.startsWith('?start')) {
         depth++;
         const isMatch = hash
@@ -54,17 +55,25 @@
         if (isMatch) {
           // Start of a range found; track it and keep walking
           startNode = node;
+          startNodeDepth = depth;
         }
       }
       // CASE 3: We have found the start and are now looking for the end tag
       else if (data.startsWith('?end') && startNode) {
-        // Only replace content if we're at a depth of 1 (in this start tag)
-        if (depth === 1) {
+        // Only replace content if we're at a depth of less than or equal to
+        // this start tag. We'd only be less than if our start tag was nested
+        // in other HTML elements and missing an end tag.
+        if (depth <= startNodeDepth) {
           const endNode = node;
 
-          // Remove everything between startNode and endNode
+          // Remove every sibling from startNode until endNode.
+          // If we don't hit the end node, then we'll remove all siblings.
           let current = startNode.nextSibling;
           while (current && current !== endNode) {
+            if (current === endNode) {
+              endNode.remove();
+              break;
+            }
             const next = current.nextSibling;
             current.remove();
             current = next;
@@ -72,19 +81,22 @@
 
           // Replace startNode with content and clean up
           startNode.replaceWith(template.content.cloneNode(true));
-          endNode.remove();
           template.remove();
           return;
         } else {
-          // If we're at a deeper depth then decrement it
+          // If we see an end tag but we're at a deeper depth then decrement
+          // the depth.
           if (depth > 1) depth--;
-          // If we're at a depth < 1 then it's a rogue `<?end>` tag so ignore it.
+          // If we're at a depth < 1, so not covered by previous two cases,
+          // then it's a rogue `<?end>` tag, so should just ignore it.
         }
       }
     }
 
+    // If we reach the end and still are depth > 0 then we're missing
+    // endNode, but can process the startNode (without removing the missing
+    // endNode).
     if (depth > 0 && startNode) {
-      // Missing endNode
 
       // Remove everything between startNode and the closing tag of the element
       let current = startNode.nextSibling;
@@ -103,7 +115,7 @@
     .querySelectorAll('template[for]')
     .forEach((t) => processTemplate(t as HTMLTemplateElement));
 
-  // Watch for, a handle newly insert templates in the HTML
+  // Watch for, and handle newly, insert templates or markers in the HTML
   const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
       for (const node of Array.from(mutation.addedNodes)) {
