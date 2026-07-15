b25d9d694d570ebf4c1cd7bf7a6108b87411ff39
Barry Pollard <barrypollard@google.com>
2026-07-15 06:05:27 +0530

    Prep for 0.1.1

--- dist/modules/template-for-polyfill.js
+++ dist/modules/template-for-polyfill.js
@@ -5,10 +5,7 @@
  */
 (() => {
     // Only process the polyfill if needed
-    if (document
-        .createRange()
-        .createContextualFragment('<?marker name=t><template for=t></template>')
-        .firstChild === null)
+    if ('htmlFor' in HTMLTemplateElement.prototype)
         return;
     console.log('Loading templates for polyfill...');
     // Helper function to actually insert the content and cleanup the
@@ -60,12 +57,14 @@
             : template.parentElement;
         // We use a TreeWalker instead of regular query selectors to
         // handle comments and processing instructions
-        const walker = document.createTreeWalker(parent,
-        // Processing Instructions usually are comments in non-supporting
-        // browser, but we also handle the case of actual Processing
-        // Instructions in case browsers ever introduce them for other
-        // reasons without shipping <template for> support
-        NodeFilter.SHOW_COMMENT | NodeFilter.SHOW_PROCESSING_INSTRUCTION);
+        const walker = document.createTreeWalker(
+          parent,
+          // Processing Instructions usually are comments in non-supporting
+          // browser, but we also handle the case of actual Processing
+          // Instructions in case browsers ever introduce them for other
+          // reasons without shipping <template for> support
+          NodeFilter.SHOW_COMMENT | NodeFilter.SHOW_PROCESSING_INSTRUCTION
+        );
         let node;
         let startNode = null;
         let depth = 0;
